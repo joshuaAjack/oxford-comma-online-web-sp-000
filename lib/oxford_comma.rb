2f09f5ae3851fd3fe921f968cd2a63ab3f6b945c
Joshua A Jack <mynameisjoshuajack@gmail.com>
2020-02-27 04:07:58 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ return array.first
 if array.size == 1 
 elsif array.size == 2 
   array.insert(1, "and").join("")
-  elsif array > 2 
+  else array > 2 
   array[-1].insert(0, "and")
   array.join(",")
 end
