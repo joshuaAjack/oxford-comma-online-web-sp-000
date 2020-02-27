bd2ac648df537a391182f361e49e75759d128c3e
Joshua A Jack <mynameisjoshuajack@gmail.com>
2020-02-27 04:07:46 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
 return array.first 
 if array.size == 1 
-if array.size == 2 
+elsif array.size == 2 
   array.insert(1, "and").join("")
   elsif array > 2 
   array[-1].insert(0, "and")
